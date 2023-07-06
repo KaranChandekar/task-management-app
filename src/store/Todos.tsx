14ba35d5eeb55beab862f372f6c460cd6894f49b
Karan Chandekar <karanchandekar431@gmail.com>
2023-07-06 21:18:45 +0530

    feat: define the handleTodo function to add data into state variable

--- src/store/Todos.tsx
+++ src/store/Todos.tsx
@@ -1,5 +1,34 @@
-import { createContext } from "react";
+import { ReactNode, createContext, useState } from "react";
+
+export type Todo = {
+  id: string;
+  task: string;
+  isCompleted: boolean;
+  createdAt: Date;
+};
+
+export type TodosContext = {
+  todos: Todo[];
+  handleAddTodo: (task: string) => void;
+};
 
 export const TodosContext = createContext(null);
 
-export const TodosProvider = (children: { children: ReactNode }) => {};
+export const TodosProvider = (children: { children: ReactNode }) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAddTodo = (task: string) => {
+    setTodos((prev) => {
+      const newTodos: Todo[] = [
+        {
+          id: Math.random().toString(),
+          task,
+          isCompleted: false,
+          createdAt: new Date(),
+        },
+        ...prev,
+      ];
+      return newTodos;
+    });
+  };
+};
